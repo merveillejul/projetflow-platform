9c4c395d862fc63206f7fb7dbd49ec3ffc0e622f
merveillejul <merveillenourryssou@outlook.fr>
2026-04-22 14:33:29 +0200

    API URL Railway production

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api"
+    baseURL: 'https://projetflow-platform-production.up.railway.app/api'
 });
 
 API.interceptors.request.use((config) => {
